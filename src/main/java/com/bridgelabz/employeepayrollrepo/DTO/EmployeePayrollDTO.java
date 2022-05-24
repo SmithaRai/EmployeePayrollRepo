c0f312c6313e92ead0ed6ac987f389b97dcb366c
unknown <SmithaRai.com>
2022-05-24 20:31:47 +0530

    Added : added remaining validations

--- src/main/java/com/bridgelabz/employeepayrollrepo/DTO/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollrepo/DTO/EmployeePayrollDTO.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.employeepayrollrepo.DTO;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -12,10 +12,17 @@ public class EmployeePayrollDTO {
     public String name;
     @Min(value = 500,message = "Min Wage should be more than 500")
     public Long salary;
+    @Pattern(regexp = "male|female",message = "Gender needs to be male or female")
     public String gender;
+    @JsonFormat(pattern = "dd MM yyyy")
+    @NotNull(message = "startDate should Not be Empty")
+    @PastOrPresent (message = "startDate should be past or todays date")
     public LocalDate startDate;
+    @NotBlank(message = "Note cannot be empty")
     public String note;
+    @NotBlank(message = "profilePic cannot be empty")
     public String profilePic;
+    @NotNull (message = "department should not be empty")
     public List<String> department;
 
 }
--- src/main/java/com/bridgelabz/employeepayrollrepo/exceptions/EmployeePayrollExceptionalHandler.java
+++ src/main/java/com/bridgelabz/employeepayrollrepo/exceptions/EmployeePayrollExceptionalHandler.java
@@ -1,8 +1,10 @@
 package com.bridgelabz.employeepayrollrepo.exceptions;
 
 import com.bridgelabz.employeepayrollrepo.DTO.ResponseDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,16 +14,26 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
+@Slf4j
 public class EmployeePayrollExceptionalHandler {
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
-        List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
-        List<String> errMesg = errorList.stream().map(objErr->objErr.getDefaultMessage()).collect(Collectors.toList());
 
-        ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request",errMesg);
+private static final String message = "Exception while processing REST Request";
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseDTO> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception){
+        log.error("Invalid Date Format",exception);
+        ResponseDTO responseDTO = new ResponseDTO(message,"Should have date in the Format dd MMM yyyy");
         return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
     }
 
+
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+            List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+            List<String> errMesg = errorList.stream().map(objErr->objErr.getDefaultMessage()).collect(Collectors.toList());
+
+            ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request",errMesg);
+            return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+        }
     @ExceptionHandler(EmployeePayrollException.class)
     public ResponseEntity<ResponseDTO> handleEmployeePayrollException(EmployeePayrollException exception) {
         ResponseDTO responseDTO = new ResponseDTO("Exception While processing REST Request", exception.getMessage());
